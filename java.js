65eb949834b1cce74738d2a693ad4a547458ccd9
broo2221 <syakipratama35@gmail.com>
2024-11-19 14:15:08 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+// Fungsi untuk memutar lagu
+function playBirthdaySong() {
+    const audio = document.getElementById('birthday-song');
+    audio.play();
+  }
+  
+  // Fungsi untuk menampilkan pesan ucapan
+  function showMessage() {
+    const messageContainer = document.getElementById('message-container');
+    messageContainer.style.display = 'block'; // Menampilkan pesan
+  }
+  
